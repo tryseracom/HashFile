4ac6c01eeaba00b799c289238cdf5dd1123cd2b8
Andrew Meyers <a.meyers@pulsetv.com>
2021-01-14 13:59:11 -0600

    Write basic file hashing program.

--- HashFile/Program.cs
+++ HashFile/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Security.Cryptography;
 
 namespace HashFile
 {
@@ -8,6 +9,33 @@ namespace HashFile
     {
         static void Main(string[] args)
         {
+            if(args.Length != 1)
+            {
+                Console.WriteLine("Usage: HashFile [FileToHash]");
+                return;
+            }
+
+            string fileName = Path.GetFileNameWithoutExtension(args[0]);
+            string extension = Path.GetExtension(args[0]);
+            string directory = Path.GetDirectoryName(args[0]);
+            string hashed = Path.Combine(directory, string.Format("{0}.hashed.{1}", fileName, extension));
+            StreamReader reader = File.OpenText(args[0]);
+            StreamWriter writer = new StreamWriter(hashed);
+            string line = null;
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            while((line = reader.ReadLine()) != null)
+            {
+                byte[] hash = md5.ComputeHash(Encoding.ASCII.GetBytes(line));
+                StringBuilder hashBuilder = new StringBuilder();
+                foreach(byte b in hash)
+                {
+                    hashBuilder.AppendFormat("{0:x2}", b);
+                }
+                writer.WriteLine(hashBuilder.ToString());
+            }
+            writer.Flush();
+            writer.Close();
+            reader.Close();
         }
     }
 }
